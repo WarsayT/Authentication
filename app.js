d773f9d4256be6ae985268e71cbcd10f70405d94
WarsayT <warsay.teklebrhan@gmail.com>
2022-07-30 00:21:49 +0400

    Add env file

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
@@ -27,8 +28,7 @@ const userSchema = new mongoose.Schema({
 });
 
 // a secret sctring to encrypt our data
-const secret = "Thisisourlittlesecret";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 // mongoose model for our users
 const User = new mongoose.model("User", userSchema);
